ba4a3cf4505e90fe5a63a914af814f101cc6d82f
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-07-13 11:43:14 +0800

    add TodoApp

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { TodoApp } from "./TodoApp";
+
+
+export class App extends Component {
+    render() {
+        return (
+            <TodoApp />
+        );
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,11 @@
-import React, {Component} from "react"
-import ReactDOM from "react-dom"
-import {speak} from "./freedom"
+import React from "react";
+import ReactDOM from "react-dom";
+import {speak} from "./freedom";
+import {App} from './App'
 
 console.log("Hello, world!")
-speak("This is an example for TypeScript file.")
+speak("This is an Todo App Demo")
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Helle, world!</h1>
-                <p>This is a template for creating single page application with React.js</p>
-                <p>Have fun coding!</p>
-            </div>
-        )
-    }
-}
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
